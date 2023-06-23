48775c2cf35b9a5a6e9926db3a7eec21bba93ff2
James Grieve <jamesonrgrieve@gmail.com>
2023-06-23 09:53:03 -0600

    Fixed recursive call;

--- forms/buttons/SubmitButton.tsx
+++ forms/buttons/SubmitButton.tsx
@@ -1,6 +1,7 @@
 import { Button, FormControl } from "@mui/material";
 
 export default function SubmitButton(props: any) {
+    console.log("Submit Button");
     return <FormControl required fullWidth sx={{ my: "1rem" }}>
         <Button variant="contained" type="submit">{props.label}</Button>
     </FormControl>;
--- forms/fields/TextField.tsx
+++ forms/fields/TextField.tsx
@@ -1,9 +1,9 @@
-import { FormControl, FormLabel, Typography } from "@mui/material";
-
+import { FormControl, FormLabel, Typography, TextField as MuiTextField } from "@mui/material";
 export default function TextField(props: any) {
+    console.log("Text Field");
     return <FormControl required fullWidth sx={{ my: "1rem" }}>
-        <FormLabel key={"label"} id={props.label}>{props.label}</FormLabel>,
-        <Typography key={"text"} variant="body1" gutterBottom>{props.desc}</Typography>,
-        <TextField key={"field"} fullWidth id={props.id} label={(props.helperText == null) ? props.label : props.helperText} variant="outlined" required />
+        <FormLabel key={"label"} id={props.label}>{props.label}</FormLabel>
+        <Typography key={"text"} variant="body1" gutterBottom>{props.desc}</Typography>
+        <MuiTextField key={"field"} fullWidth id={props.id} label={props.helperText??props.label} variant="outlined" required />
     </FormControl>;
 }
