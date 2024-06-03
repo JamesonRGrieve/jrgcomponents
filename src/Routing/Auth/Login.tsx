6d2dd84baa3178b7df2c3cff40593623029a5f88
James Grieve <jamesonrgrieve@gmail.com>
2024-06-03 13:12:02 -0600

    Add regex for start and end of string;

--- src/Routing/Auth/Login.tsx
+++ src/Routing/Auth/Login.tsx
@@ -22,7 +22,7 @@ export default function Login({ searchParams }: { searchParams: any }): ReactNod
         setResponseMessage(response.data.detail);
       } else {
         if (
-          /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/.test(
+          /^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)$/.test(
             response.data.detail,
           )
         ) {
