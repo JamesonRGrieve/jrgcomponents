0e083ac5a0aab28cb73904b39d6e6711d4967492
James Grieve <jamesonrgrieve@gmail.com>
2024-06-10 11:29:16 -0600

    Forward services;

--- src/Routing/Auth/Router.tsx
+++ src/Routing/Auth/Router.tsx
@@ -5,12 +5,20 @@ import Login from './Login';
 import Manage from './Manage';
 import Register from './Register';
 import { notFound } from 'next/navigation';
-export default function AuthRouter({ params, searchParams }: { params: { slug?: string[] }; searchParams: any }) {
+export default function AuthRouter({
+  services,
+  params,
+  searchParams,
+}: {
+  services: object;
+  params: { slug?: string[] };
+  searchParams: any;
+}) {
   const { slug } = params;
   const pages = {
     login: <Login searchParams={searchParams} />,
     manage: <Manage />,
-    register: <Register services={{}} />,
+    register: <Register services={services} />,
   };
   if (!slug) return <User />;
   else {
