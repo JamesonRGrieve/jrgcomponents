cf67d6f0b806cf567e300141eda623c956db362f
James Grieve <jamesonrgrieve@gmail.com>
2024-06-13 12:23:06 -0600

    Add OAuth providers;

--- src/Routing/Auth/OAuth.tsx
+++ src/Routing/Auth/OAuth.tsx
@@ -1,41 +1,25 @@
 'use client';
 
-import React, { ReactNode } from 'react';
+import React, { ReactNode, useCallback } from 'react';
 
 import OAuth2Login from 'react-simple-oauth2-login';
-import { GitHub, Google, Microsoft } from '@mui/icons-material';
-import IconButton from '../../MUI/Styled/Button/IconButton';
-const onOAuth2Success = (response: any) => console.log(response);
-const onOAuth2Failure = (response: any) => console.error(response);
 
-const providers = {
-  GitHub: {
-    client_id: process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID,
-    scope: 'user:email',
-    uri: '',
-    params: {},
-    icon: <GitHub />,
-  },
-  Google: {
-    client_id: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
-    scope: 'profile email https://www.googleapis.com/auth/gmail.send',
-    uri: 'https://accounts.google.com/o/oauth2/v2/auth',
-    params: {
-      access_type: 'offline',
-    },
-    icon: <Google />,
-  },
-  Microsoft: {
-    client_id: process.env.NEXT_PUBLIC_MICROSOFT_CLIENT_ID,
-    scope: 'https://graph.microsoft.com/User.Read https://graph.microsoft.com/Mail.Send',
-    uri: '',
-    params: {},
-    icon: <Microsoft />,
-  },
-};
+import IconButton from '../../MUI/Styled/Button/IconButton';
+import { useRouter } from 'next/navigation';
+import { getCookie } from 'cookies-next';
+
+import providers from './OAuthProviders';
 
 export default function Identify(): ReactNode {
   console.log('OAuth.tsx: Identify()', process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID);
+  const router = useRouter();
+  const onOAuth2 = useCallback(
+    (response: any) => {
+      console.log('CLOSED POPUP');
+      router.push(getCookie('href') ?? '/');
+    },
+    [router],
+  );
   return (
     <>
       {Object.values(providers).some((provider) => provider.client_id) && <hr />}
@@ -49,8 +33,8 @@ export default function Identify(): ReactNode {
               clientId={provider.client_id}
               scope={provider.scope}
               redirectUri={`http://localhost:3437/user/close/${key.toLowerCase()}`}
-              onSuccess={onOAuth2Success}
-              onFailure={onOAuth2Failure}
+              onSuccess={onOAuth2}
+              onFailure={onOAuth2}
               extraParams={provider.params}
               isCrossOrigin
               render={(renderProps) => (
