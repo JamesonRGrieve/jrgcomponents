f73c8748b9ddd8b3d79607c713f2c85fc3b36986
James Grieve <jamesonrgrieve@gmail.com>
2024-07-05 12:33:29 -0600

    Update ImageDialog to support non-next images;

--- src/Dialog/Image/ImageDialog.tsx
+++ src/Dialog/Image/ImageDialog.tsx
@@ -6,29 +6,45 @@ import { Box } from '@mui/material';
 export type ImageDialogProps = {
   imageSrc: string;
   imageThumbnail?: ReactNode;
+  nextImage?: boolean;
 } & CommonDialogProps;
 
-const ImageDialog: React.FC<ImageDialogProps> = ({ imageSrc, title, imageThumbnail = ImageThumbnail, ...props }) => {
+const ImageDialog: React.FC<ImageDialogProps> = ({
+  imageSrc,
+  nextImage = true,
+  title,
+  imageThumbnail = ImageThumbnail,
+  ...props
+}) => {
   return (
     <Dialog
       title={title}
       content={
         <Box width='100%' height='100%' minWidth='10rem' minHeight='10rem'>
-          <Image src={imageSrc} alt={title} fill style={{ objectFit: 'contain' }} />
+          {nextImage ? (
+            <Image src={imageSrc} alt={title} fill style={{ objectFit: 'contain' }} />
+          ) : (
+            <img src={imageSrc} alt={title} style={{ objectFit: 'contain' }} />
+          )}
         </Box>
       }
       ButtonComponent={imageThumbnail}
       ButtonProps={{
         src: imageSrc,
         alt: title,
+        nextImage: nextImage,
       }}
     />
   );
 };
-const ImageThumbnail = ({ src, alt, ...props }: { src: string; alt: string }) => {
+const ImageThumbnail = ({ src, alt, nextImage, ...props }: { src: string; alt: string; nextImage: boolean }) => {
   return (
     <Box position='relative' width='4rem' height='4rem' sx={{ cursor: 'pointer' }} {...props}>
-      <Image src={src} alt={alt} fill style={{ objectFit: 'contain' }} />
+      {nextImage ? (
+        <Image src={src} alt={alt} fill style={{ objectFit: 'contain' }} />
+      ) : (
+        <img src={src} alt={alt} style={{ objectFit: 'contain' }} />
+      )}
     </Box>
   );
 };
