50ce56591bf6dbf4ce68a45dc50a2e336652c02b
Bilal (Birm) Wais <b.wais@outlook.com>
2024-08-12 13:38:37 -0600

    fix mui select label bug

--- src/MUI/Styled/Input/SelectField.tsx
+++ src/MUI/Styled/Input/SelectField.tsx
@@ -1,5 +1,5 @@
 import { MenuItem, Select } from '@mui/material';
-import React from 'react';
+import React, { useState } from 'react';
 
 interface SelectFieldProps {
   id: string;
@@ -11,8 +11,21 @@ interface SelectFieldProps {
 }
 
 export default function SelectField({ id, value, onChange, items, name, label }: SelectFieldProps) {
+  const [isFocused, setIsFocused] = useState(false);
+
   return (
-    <Select id={id} value={value} onChange={onChange} label={label} name={name} displayEmpty>
+    <Select
+      id={id}
+      labelId={`${id}-label`}
+      value={value}
+      onChange={onChange}
+      label={label}
+      name={name}
+      onFocus={() => setIsFocused(true)}
+      onBlur={() => setIsFocused(false)}
+      notched={isFocused}
+      displayEmpty
+    >
       {items?.map((item: any, index: number) => (
         <MenuItem key={index} value={item.value ?? item}>
           {item.label ?? item}
