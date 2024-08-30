b177500367a97b249cacf96401982972d07b6d9d
James Grieve <jamesonrgrieve@gmail.com>
2024-08-30 12:51:10 -0600

    Fix and/or;

--- src/Routing/Auth/Router.tsx
+++ src/Routing/Auth/Router.tsx
@@ -40,7 +40,7 @@ export type AuthenticationConfig = {
 
 export const useAuthentication = () => {
   const context = useContext(AuthenticationContext);
-  assert(!context.authModes.basic && !context.authModes.magical, 'Basic and Magical modes cannot both be enabled.');
+  assert(!context.authModes.basic || !context.authModes.magical, 'Basic and Magical modes cannot both be enabled.');
   if (context === undefined) {
     throw new Error('useAuthentication must be used within an AuthenticationProvider');
   }
