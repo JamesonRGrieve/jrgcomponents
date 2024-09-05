1b108b847a8bb441870f997584356a5707882466
Bilal (Birm) Wais <b.wais@outlook.com>
2024-09-05 02:24:00 -0600

    update dymanic forms to use new inputs

--- src/MUI/Styled/Input/TextField.tsx
+++ src/MUI/Styled/Input/TextField.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Input } from '../../../components/ui/input';
 import { Label } from '../../../components/ui/label';
 
-interface TextFieldProps {
+interface TextFieldProps extends React.InputHTMLAttributes<HTMLInputElement> {
   id: string;
   value: string;
   onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
@@ -26,11 +26,12 @@ export default function TextField({
   autoComplete,
   className,
   type = 'text',
+  ...props
 }: TextFieldProps) {
   return (
     <div className='flex flex-col w-full gap-2 mb-4'>
       <Label htmlFor={id}>{label}</Label>
-      <Input {...{ id, value, onChange, name, autoComplete, placeholder, className, type }} required />
+      <Input {...{ id, value, onChange, name, autoComplete, placeholder, className, type, ...props }} required />
       {helperText && <p className='text-sm text-gray-500'>{helperText}</p>}
     </div>
   );
