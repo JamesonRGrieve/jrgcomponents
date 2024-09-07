397d31c7ebbd7febcf545afa377dc416c7197ed4
Bilal (Birm) Wais <b.wais@outlook.com>
2024-09-07 04:17:26 -0600

    import alias skill issue

--- src/components/ui/command.tsx
+++ src/components/ui/command.tsx
@@ -6,7 +6,7 @@ import { Command as CommandPrimitive } from 'cmdk';
 import { Search } from 'lucide-react';
 
 import { cn } from '../../lib/utils';
-import { Dialog, DialogContent } from 'src/components/ui/dialog';
+import { Dialog, DialogContent } from './dialog';
 
 const Command = React.forwardRef<
   React.ElementRef<typeof CommandPrimitive>,
@@ -25,7 +25,7 @@ interface CommandDialogProps extends DialogProps {}
 const CommandDialog = ({ children, ...props }: CommandDialogProps) => {
   return (
     <Dialog {...props}>
-      <DialogContent className='overflow-hidden p-0 shadow-lg'>
+      <DialogContent className='p-0 overflow-hidden shadow-lg'>
         <Command className='[&_[cmdk-group-heading]]:px-2 [&_[cmdk-group-heading]]:font-medium [&_[cmdk-group-heading]]:text-muted-foreground [&_[cmdk-group]:not([hidden])_~[cmdk-group]]:pt-0 [&_[cmdk-group]]:px-2 [&_[cmdk-input-wrapper]_svg]:h-5 [&_[cmdk-input-wrapper]_svg]:w-5 [&_[cmdk-input]]:h-12 [&_[cmdk-item]]:px-2 [&_[cmdk-item]]:py-3 [&_[cmdk-item]_svg]:h-5 [&_[cmdk-item]_svg]:w-5'>
           {children}
         </Command>
@@ -38,8 +38,8 @@ const CommandInput = React.forwardRef<
   React.ElementRef<typeof CommandPrimitive.Input>,
   React.ComponentPropsWithoutRef<typeof CommandPrimitive.Input>
 >(({ className, ...props }, ref) => (
-  <div className='flex items-center border-b px-3' cmdk-input-wrapper=''>
-    <Search className='mr-2 h-4 w-4 shrink-0 opacity-50' />
+  <div className='flex items-center px-3 border-b' cmdk-input-wrapper=''>
+    <Search className='w-4 h-4 mr-2 opacity-50 shrink-0' />
     <CommandPrimitive.Input
       ref={ref}
       className={cn(
@@ -65,7 +65,7 @@ CommandList.displayName = CommandPrimitive.List.displayName;
 const CommandEmpty = React.forwardRef<
   React.ElementRef<typeof CommandPrimitive.Empty>,
   React.ComponentPropsWithoutRef<typeof CommandPrimitive.Empty>
->((props, ref) => <CommandPrimitive.Empty ref={ref} className='py-6 text-center text-sm' {...props} />);
+>((props, ref) => <CommandPrimitive.Empty ref={ref} className='py-6 text-sm text-center' {...props} />);
 
 CommandEmpty.displayName = CommandPrimitive.Empty.displayName;
 
